5592cf7fb3c282d3de270179f804fd4dd0ec0cb6
ekaone <ekaone3033@gmail.com>
2021-02-23 10:47:27 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Particles from "react-particles-js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Particles width={500} height={500} />
+      Particles JS
+    </div>
+  );
+}
+
+export default App;
